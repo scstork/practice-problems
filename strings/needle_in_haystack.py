f1b51d1b4d11377653a2c4729f6a31e89a5c1bf9
Shannon Stork <scs94@case.edu>
2021-03-14 20:40:01 -0400

    add needle in haystack solution

---
+++ strings/needle_in_haystack.py
@@ -0,0 +1,26 @@
+class Solution:
+    """
+    Implement strStr().
+
+    Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+    
+    If needle is an empty string, return 0.
+    """
+
+    def strStr(self, haystack: str, needle: str) -> int:
+        if needle == "":
+            return 0
+        # check if first len(needle) characters in haystack == needle
+        # break as soon as we know they aren't equal
+        # this should be faster than the builtin str starts with method
+        orig_index = 0
+        while len(haystack) >= len(needle):
+            index = 0
+            while index < len(needle) and haystack[index] == needle[index]:
+                index += 1
+            if index == len(needle):
+                return orig_index
+            orig_index += 1
+            haystack = haystack[1:]
+        return -1
+        
